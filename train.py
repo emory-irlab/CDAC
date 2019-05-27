00a6dc60cd52e1b6f68c0b7d8499b56544183414
Ali Ahmadvand <ali.ahmadvand.66@gmail.com>
2019-05-27 18:35:02 -0400

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import os, warnings, sys, re
 import time, pickle
 import datetime
 import data_helpers
-from cdac import CDAC
+from cdac import ContextualDAC
 from tensorflow.contrib import learn
 from sklearn.utils import shuffle
 from sklearn.preprocessing import normalize
@@ -163,7 +163,7 @@ with tf.Graph().as_default():
       log_device_placement=FLAGS.log_device_placement)
     sess = tf.Session(config=session_conf)
     with sess.as_default():
-        cnn = CDAC(
+        cnn = ContextualDAC(
             sequence_length=x_train.shape[1],
             num_classes=42,
             vocab_size=len(vocab),
